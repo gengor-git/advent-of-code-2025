202c48e3c2a3893a96fe7c075334ec6cd1603928
Martin Palmowski <martin.palmowski@gmail.com>
2025-12-01 16:27:09 +0100

    Works only partially.

--- day01/calculate_password.py
+++ day01/calculate_password.py
@@ -32,31 +32,38 @@ def calculate_password2(data_file) -> int:
     start_position = 50
     current_position = start_position
     movement = {'L': int(-1), 'R': int(1)}
+    rotations = 0    
 
     for entry in data:
-        clicks = current_position + (movement[entry[0]] * entry[1])
+        clicks = (movement[entry[0]] * entry[1])
+        rotations = 0
 
         # calculate the relative position on the dial
         last_position = current_position
-        current_position = clicks
+        current_position = current_position + clicks
 
         if (current_position == 0):
             # we are exactly at 0
-            result += 1
-        else:
-            while (current_position < 0):
-                current_position = current_position + 100
-                if (last_position != 0 or clicks > 100):
-                    result += 1
-            while (current_position > 99):
-                current_position = current_position - 100
-                result += 1
-                
+            rotations += 1
+        while (current_position < 0):
+            # left way is more difficult
+            current_position = current_position + 100
+            if (last_position != 0):
+                # simple version, we know we passed 0 at least once
+                rotations += 1
+            elif (last_position == 0 and clicks < -99):
+                rotations += 1
+        while (current_position > 99):
+            current_position = current_position - 100
+            rotations += 1
 
+        result += rotations
+        print("{:3} -> {:3} via {}{:3} with {:3} rotations".format(last_position, current_position, entry[0], entry[1], rotations))
+                
     return result
 
 if __name__ == "__main__":
     # print(calculate_password(sample_file))
     # print(calculate_password(input_file))
-    print(calculate_password2(sample_file))
-    # print(calculate_password2(input_file))
+    # print(calculate_password2(sample_file))
+    print(calculate_password2(input_file))
