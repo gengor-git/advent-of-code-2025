1ca922000304f1614b960c63599dba63947eb82c
Martin Palmowski <martin.palmowski@gmail.com>
2025-12-04 16:24:49 +0100

    Day 04 part 2 solved.

--- day04/calculate_paperrolls.py
+++ day04/calculate_paperrolls.py
@@ -20,6 +20,21 @@ def calculate_paperrolls_pt1(data_file) -> int:
         
     return result
 
+
+def calculate_paperrolls_pt2(data_file) -> int:
+    result = 0
+    data = [list(line) for line in open(data_file).read().strip().splitlines()]
+
+    grid = {
+        (x, y): data[y][x]
+        for y in range(len(data))
+        for x in range(len(data[0]))
+    }    
+    while ((removal := sum_and_remove_accessible_rolls(grid)) > 0):
+        result += removal
+     
+    return result
+
 def sum_accessible_rolls(grid: dict) -> int:
     result = 0
     for pos, value in grid.items():
@@ -28,12 +43,16 @@ def sum_accessible_rolls(grid: dict) -> int:
 
     return result
 
-# def has_neighbor_value_dict(grid: dict, pos: tuple, target: str):
-#     x, y = pos
-#     for dx, dy in DIRS:
-#         if grid.get((x+dx, y+dy)) == target:
-#             return True
-#     return False
+def sum_and_remove_accessible_rolls(grid: dict) -> int:
+    result = 0
+    for pos, value in grid.items():
+        # print('Coord {}, {}'.format(pos, value))
+        if (value == "@" and count_neighbor_values(grid, pos, "@") < 4):
+            result += 1
+            grid[pos] = "."
+        
+    return result
+
 
 def count_neighbor_values(grid: dict, pos: tuple[int, int], target: str) -> int:
     x, y = pos
@@ -45,15 +64,8 @@ def count_neighbor_values(grid: dict, pos: tuple[int, int], target: str) -> int:
     return count
 
 
-def calculate_something2(data_file) -> int:
-    result = 0
-    data = [[int(s) for s in line.split()] for line in open(data_file).read().strip().splitlines()]
-    for entry in data:
-        pass
-    return result
-
 if __name__ == "__main__":
     # print(calculate_paperrolls_pt1(sample_file))
-    print(calculate_paperrolls_pt1(input_file))
-    # print(calculate_something2(sample_file))
-    # print(calculate_something2(input_file))
+    # print(calculate_paperrolls_pt1(input_file))
+    # print(calculate_paperrolls_pt2(sample_file))
+    print(calculate_paperrolls_pt2(input_file))
--- day04/test_calculate_paperrolls.py
+++ day04/test_calculate_paperrolls.py
@@ -10,8 +10,8 @@ class TestCalculatePaperolls(unittest.TestCase):
         self.assertEqual(this_day.calculate_paperrolls_pt1(this_day.input_file), 1451)
         
     def testSamplePart2(self):
-        self.assertEqual(this_day.calculate_something2(this_day.sample_file), 11)
+        self.assertEqual(this_day.calculate_paperrolls_pt2(this_day.sample_file), 43)
 
     def testInputPart2(self):
-        self.assertEqual(this_day.calculate_something2(this_day.input_file), 3246517)
+        self.assertEqual(this_day.calculate_paperrolls_pt2(this_day.input_file), 8701)
         
